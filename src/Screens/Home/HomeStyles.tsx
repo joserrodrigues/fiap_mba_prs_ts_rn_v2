d25ad593e473068528228253a009b6aed4d85f2f
rubens Rodrigues <rubens@schoolguardian.app>
2022-10-19 01:13:49 -0300

    Class 2 - Section 3 - Examples

--- src/Screens/Home/HomeStyles.tsx
+++ src/Screens/Home/HomeStyles.tsx
@@ -1,19 +1,61 @@
 import { StyleSheet } from "react-native";
+import styled from "styled-components/native";
+import { Button, Image } from "react-native-elements";
 
-export default StyleSheet.create({
-  container: {
+export const MainContainer = styled.View`
+  flex: 1;
+`;
+
+export const TopScreen = styled.View`
+  flex: 5;
+`;
+
+export const BottomScreen = styled.View`
+  flex: 5;
+`;
+
+export const TitlePage = styled.Text`
+  margin: 20px;
+  font-size: 30px;
+  text-align: center;
+  color: red;
+`;
+
+export const StyledImage = styled(Image).attrs({
+  containerStyle: {
+    width: "100%",
+    height: "100%",
     flex: 1,
   },
-  blockA: {
-    flex: 1,
+})``;
+
+export const TextBoxContainer = styled.View`
+  flex-grow: 1;
+`;
+
+export const TextBlock = styled.View`
+  flex: 1;
+  margin: 15px;
+  border-radius: 15px;
+  border-width: 1px;
+  border-color: red;
+  padding: 20px;
+`;
+
+export const BottomButton = styled.View`
+  height: 60px;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+export const StyledButton = styled(Button).attrs({
+  buttonStyle: {
     backgroundColor: "red",
+    borderRadius: 10,
   },
-  blockB: {
-    flex: 2,
-    backgroundColor: "green",
+  containerStyle: {
+    width: "35%",
+    marginHorizontal: 20,
+    marginVertical: 10,
   },
-  blockC: {
-    flex: 3,
-    backgroundColor: "blue",
-  },
-});
+})``;
--- src/Screens/Home/HomeView.tsx
+++ src/Screens/Home/HomeView.tsx
@@ -1,14 +1,60 @@
 import React from "react";
-import { View } from "react-native";
-import styles from "./HomeStyles";
+import { View, Text } from "react-native";
+import {
+  BottomButton,
+  BottomScreen,
+  MainContainer,
+  StyledButton,
+  StyledImage,
+  TextBlock,
+  TextBoxContainer,
+  TitlePage,
+  TopScreen,
+} from "./HomeStyles";
 
 const HomeView = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.blockA}></View>
-      <View style={styles.blockB}></View>
-      <View style={styles.blockC}></View>
-    </View>
+    <MainContainer>
+      <TopScreen>
+        <View>
+          <TitlePage> Título da Página</TitlePage>
+        </View>
+        <StyledImage
+          source={{
+            uri: "https://thecatapi.com/api/images/get?format=src&type=jpg",
+          }}
+        />
+      </TopScreen>
+
+      <BottomScreen>
+        <TextBoxContainer>
+          <TextBlock>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae
+              semper quis lectus nulla at volutpat diam. Malesuada bibendum arcu
+              vitae elementum curabitur vitae nunc. Nibh sit amet commodo nulla.
+              Massa sed elementum tempus egestas sed sed. Eget sit amet tellus
+              cras. Magna ac placerat vestibulum lectus. Sit amet venenatis urna
+              cursus. Est placerat in egestas erat imperdiet sed euismod nisi.
+              Duis at tellus at urna condimentum mattis. Non blandit massa enim
+              nec. Vestibulum lectus mauris ultrices eros in cursus. Consequat
+              id porta nibh venenatis cras sed felis eget. Nunc mi ipsum
+              faucibus vitae aliquet nec. Sapien nec sagittis aliquam malesuada
+              bibendum arcu. Tristique sollicitudin nibh sit amet. Rhoncus
+              mattis rhoncus urna neque viverra justo nec ultrices. Cursus
+              mattis molestie a iaculis at erat pellentesque adipiscing. Neque
+              ornare aenean euismod elementum nisi quis eleifend quam
+              adipiscing.
+            </Text>
+          </TextBlock>
+        </TextBoxContainer>
+        <BottomButton>
+          <StyledButton title="Anterior" />
+          <StyledButton title="Próximo" />
+        </BottomButton>
+      </BottomScreen>
+    </MainContainer>
   );
 };
 
