8a7d88d9047d7ae991ec3b1d0eee6b7cbd313a0b
rubens Rodrigues <rubens@schoolguardian.app>
2022-10-18 23:55:17 -0300

    Class 1 - Section 3 - React Native Elements

--- App.tsx
+++ App.tsx
@@ -1,26 +1,70 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { AirbnbRating, Button, Avatar, Badge } from "react-native-elements";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.textInfo}>Hello World</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 30,
+        }}
+      >
+        <AirbnbRating
+          count={10}
+          reviews={[
+            "Terrible",
+            "Bad",
+            "Meh",
+            "OK",
+            "Good",
+            "Hmm...",
+            "Very Good",
+            "Wow",
+            "Amazing",
+            "Unbelievable",
+          ]}
+          defaultRating={10}
+          size={20}
+        />
+        <View>
+          <Avatar
+            rounded
+            source={{ uri: "https://randomuser.me/api/portraits/men/41.jpg" }}
+            size={124}
+          />
+          <Badge
+            value="99+"
+            status="error"
+            containerStyle={{ position: "absolute", top: 5, left: 0 }}
+          />
+        </View>
+        <Button
+          title={"React Native Elements"}
+          containerStyle={{
+            width: 200,
+            marginHorizontal: 50,
+            marginVertical: 10,
+          }}
+        />
+      </View>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    fontSize: 40,
-    alignItems: "center",
-    justifyContent: "center",
+    marginTop: 40,
   },
-  textInfo: {
-    fontSize: 35,
-    fontWeight: "bold",
-    color: "red",
+  headingContainer: {
+    paddingTop: 50,
+  },
+  rating: {
+    paddingVertical: 10,
   },
 });
